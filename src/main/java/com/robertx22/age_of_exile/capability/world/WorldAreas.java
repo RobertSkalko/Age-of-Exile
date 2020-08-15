913c9fe8ff1003fe61289d83539ca04054231809
robert trebor <treborx555@gmail.com>
2020-08-15 18:52:04 +0200

    making it work

--- src/main/java/com/robertx22/age_of_exile/capability/world/WorldAreas.java
+++ src/main/java/com/robertx22/age_of_exile/capability/world/WorldAreas.java
@@ -37,15 +37,21 @@ public class WorldAreas implements Component {
             Watch watch = new Watch();
 
             Set<ChunkPos> matches = new HashSet<>();
-            Stack<ChunkPos> chunks = new Stack<>();
+            Stack<ChunkPos> stack = new Stack<>();
             Set<ChunkPos> visited = new HashSet<>();
 
             Biome biome = world.getBiome(pos);
 
-            branchTo(world, biome, matches, chunks, visited, new ChunkPos(pos));
+            branchTo(world, biome, matches, stack, visited, new ChunkPos(pos));
 
-            while (!chunks.isEmpty()) {
-                branchTo(world, biome, matches, chunks, visited, chunks.pop());
+            while (!stack.isEmpty()) {
+                ChunkPos cp = stack.pop();
+                branchTo(world, biome, matches, stack, visited, cp);
+
+                if (visited.size() > 2000) {
+                    System.out.println("Biome too big, stopping");
+                    break;
+                }
             }
 
             worldAreas.createNewArea(biome, new ArrayList<>(matches));
@@ -57,42 +63,60 @@ public class WorldAreas implements Component {
         return worldAreas.getArea(pos);
     }
 
-    static void branchTo(World world, Biome biome, Set<ChunkPos> matches, Stack<ChunkPos> chunks, Set<ChunkPos> visited, ChunkPos cp) {
+    static boolean areSameBiomes(World world, ChunkPos cp, Biome biome) {
 
-        if (isSameBiome(world, biome, cp) && !visited.contains(cp)) {
-            chunks.push(cp);
-            matches.add(cp);
-            visited.add(cp);
+        Biome other = world.getBiome(cp.getCenterBlockPos());
+
+        if (biome == other) {
+            return true;
         }
 
-        ChunkPos left = new ChunkPos(cp.x - 1, cp.z);
-        ChunkPos right = new ChunkPos(cp.x + 1, cp.z);
-        ChunkPos up = new ChunkPos(cp.x, cp.z - 1);
-        ChunkPos down = new ChunkPos(cp.x, cp.z + 1);
-
-        isSameBiomeAdd(world, biome, left, matches, chunks, visited);
-        isSameBiomeAdd(world, biome, right, matches, chunks, visited);
-        isSameBiomeAdd(world, biome, up, matches, chunks, visited);
-        isSameBiomeAdd(world, biome, down, matches, chunks, visited);
+        return biome.getCategory()
+            .equals(other.getCategory());
     }
 
-    static void isSameBiomeAdd(World world, Biome biome, ChunkPos cp, Set<ChunkPos> matches, Stack<ChunkPos> chunks, Set<ChunkPos> visited) {
-        boolean same = world.getBiome(cp.getCenterBlockPos()) == biome;
+    static void branchTo(World world, Biome biome, Set<ChunkPos> matches, Stack<ChunkPos> stack, Set<ChunkPos> visited, ChunkPos cp) {
+
+        if (areSameBiomes(world, cp, biome)) {
+
+            shouldAdd(world, biome, cp, matches, stack, visited);
+
+            ChunkPos left = new ChunkPos(cp.x - 1, cp.z);
+            ChunkPos right = new ChunkPos(cp.x + 1, cp.z);
+            ChunkPos up = new ChunkPos(cp.x, cp.z - 1);
+            ChunkPos down = new ChunkPos(cp.x, cp.z + 1);
+            ChunkPos d1 = new ChunkPos(cp.x + 1, cp.z + 1);
+            ChunkPos d2 = new ChunkPos(cp.x - 1, cp.z - 1);
+            ChunkPos d3 = new ChunkPos(cp.x + 1, cp.z - 1);
+            ChunkPos d4 = new ChunkPos(cp.x - 1, cp.z + 1);
+
+            shouldAdd(world, biome, left, matches, stack, visited);
+            shouldAdd(world, biome, right, matches, stack, visited);
+            shouldAdd(world, biome, up, matches, stack, visited);
+            shouldAdd(world, biome, down, matches, stack, visited);
+            shouldAdd(world, biome, d1, matches, stack, visited);
+            shouldAdd(world, biome, d2, matches, stack, visited);
+            shouldAdd(world, biome, d3, matches, stack, visited);
+            shouldAdd(world, biome, d4, matches, stack, visited);
+
+        }
+    }
+
+    static boolean shouldAdd(World world, Biome biome, ChunkPos cp, Set<ChunkPos> matches, Stack<ChunkPos> stack, Set<ChunkPos> visited) {
+        boolean sameBiome = areSameBiomes(world, cp, biome);
 
         if (visited.contains(cp)) {
-            return;
+            return false;
         }
 
-        if (same) {
-            chunks.add(cp);
+        if (sameBiome) {
+            stack.add(cp);
             matches.add(cp);
         }
+
         visited.add(cp);
 
-    }
-
-    static boolean isSameBiome(World world, Biome biome, ChunkPos cp) {
-        return world.getBiome(cp.getCenterBlockPos()) == biome;
+        return sameBiome;
     }
 
     Data data = new Data();
@@ -113,6 +137,11 @@ public class WorldAreas implements Component {
         data.uuid = UUID.randomUUID()
             .toString();
         data.setChunks(chunks);
+
+        this.data.areas.add(data);
+
+        data.getChunks()
+            .forEach(x -> map.put(x, data));
 
         return data;
     }
