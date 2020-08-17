e03786ae5f7278f75b098786e687d76b12b67a2e
robert trebor <treborx555@gmail.com>
2020-08-17 09:12:46 +0200

    added some more area mods, need to rework them a bit

--- src/main/java/com/robertx22/age_of_exile/areas/area_modifiers/AreaModifiers.java
+++ src/main/java/com/robertx22/age_of_exile/areas/area_modifiers/AreaModifiers.java
@@ -4,6 +4,7 @@ import com.robertx22.age_of_exile.database.data.StatModifier;
 import com.robertx22.age_of_exile.database.data.affixes.Affix;
 import com.robertx22.age_of_exile.database.data.stats.types.generated.WeaponDamage;
 import com.robertx22.age_of_exile.database.data.stats.types.misc.ExtraMobDropsStat;
+import com.robertx22.age_of_exile.database.data.stats.types.resources.LifeOnHit;
 import com.robertx22.age_of_exile.uncommon.enumclasses.Elements;
 import com.robertx22.age_of_exile.uncommon.enumclasses.ModType;
 import com.robertx22.age_of_exile.uncommon.utilityclasses.RandomUtils;
@@ -60,8 +61,12 @@ public class AreaModifiers {
             new StatModifier(1, 1, new WeaponDamage(Elements.Thunder), ModType.FLAT),
             new StatModifier(10, 10, ExtraMobDropsStat.getInstance(), ModType.FLAT)
         );
-    public AreaModifier OF_SLIMES = of("of_slimes", 1000, Affix.Type.suffix, "Of Slimes",
+    public AreaModifier OF_SLIMES = of("of_slimes", 500, Affix.Type.suffix, "Of Slimes",
         Arrays.asList(ENTITIES.THUNDER_SLIME, ENTITIES.WATER_SLIME, ENTITIES.NATURE_SLIME),
+        x -> x.getCategory() == Category.BEACH);
+
+    public AreaModifier DESOLATE = of("desolate", 1000, Affix.Type.prefix, "Desolate",
+        Arrays.asList(),
         x -> x.getCategory() == Category.BEACH);
 
     public AreaModifier ARCANE = of("arcane", 1000, Affix.Type.prefix, "Arcane",
@@ -71,6 +76,18 @@ public class AreaModifiers {
     public AreaModifier APOCALYPTIC = of("apocalyptic", 500, Affix.Type.prefix, "Apocalyptic",
         Arrays.asList(ENTITIES.FIRE_ZOMBIE, ENTITIES.ARCANE_ZOMBIE, ENTITIES.THUNDER_ZOMBIE, ENTITIES.WATER_ZOMBIE, ENTITIES.NATURE_ZOMBIE),
         x -> x.getCategory() == Category.PLAINS || x.getCategory() == Category.NETHER || x.getCategory() == Category.FOREST || x.getCategory() == Category.MUSHROOM);
+
+    public AreaModifier SPIDERS = of("spider_den", 750, Affix.Type.prefix, "Spider Den",
+        Arrays.asList(ENTITIES.ARCANE_SPIDER, ENTITIES.THUNDER_SPIDER, ENTITIES.NATURE_SPIDER, ENTITIES.FIRE_SPIDER, ENTITIES.WATER_SPIDER),
+        x -> x.getCategory() != Category.ICY && x.getCategory() != Category.PLAINS && x.getCategory() != Category.MESA && x.getCategory() != Category.RIVER);
+
+    public AreaModifier OF_VAMPIRISM = of("of_vampirism", 500, Affix.Type.suffix, "Of Vampirism",
+        Arrays.asList(),
+        x -> x.getCategory() != Category.OCEAN)
+        .addStats(
+            new StatModifier(1.5F, 1.5F, LifeOnHit.getInstance(), ModType.FLAT),
+            new StatModifier(15, 15, ExtraMobDropsStat.getInstance(), ModType.FLAT)
+        );
 
     AreaModifier of(String id, int weight, Affix.Type affixType, String locName, List<EntityType> mobSpawns, Predicate<Biome> canUseBiome) {
 
