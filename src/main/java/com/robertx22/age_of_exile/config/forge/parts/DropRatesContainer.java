473471076542d488497605cd210c7e30293111b3
robert trebor <treborx555@gmail.com>
2020-08-21 13:37:11 +0200

    remove unused

--- src/main/java/com/robertx22/age_of_exile/config/forge/parts/DropRatesContainer.java
+++ src/main/java/com/robertx22/age_of_exile/config/forge/parts/DropRatesContainer.java
@@ -4,9 +4,7 @@ public class DropRatesContainer {
 
     public double GEAR_DROPRATE = 8;
     public double SKILL_GEM_DROPRATE = 2;
-    public double JEWEL_DROPRATE = 1.25F;
-    public double UNIQUE_DROPRATE = 0.25;
+    public double JEWEL_DROPRATE = 1;
     public double CURRENCY_DROPRATE = 1;
-    public double COMPATIBLE_ITEMS_DROPRATE = 0;
 
 }
--- src/main/java/com/robertx22/age_of_exile/saveclasses/gearitem/gear_bases/GearItemEnum.java
+++ src/main/java/com/robertx22/age_of_exile/saveclasses/gearitem/gear_bases/GearItemEnum.java
@@ -1,21 +1,10 @@
 package com.robertx22.age_of_exile.saveclasses.gearitem.gear_bases;
 
-import com.robertx22.age_of_exile.config.forge.ModConfig;
-import com.robertx22.age_of_exile.uncommon.interfaces.IWeighted;
 import com.robertx22.age_of_exile.uncommon.localization.Words;
-import com.robertx22.age_of_exile.uncommon.utilityclasses.RandomUtils;
 
-import java.util.Arrays;
-
-public enum GearItemEnum implements IWeighted {
+public enum GearItemEnum {
 
     NORMAL() {
-        @Override
-        public int Weight() {
-            return (int) (ModConfig.get().DropRates.GEAR_DROPRATE
-                * 100); // cus some config values are < 1 like 0.1F
-        }
-
         @Override
         public boolean canGetAffixes() {
             return true;
@@ -33,12 +22,6 @@ public enum GearItemEnum implements IWeighted {
     },
 
     UNIQUE() {
-        @Override
-        public int Weight() {
-            return (int) (ModConfig.get().DropRates.UNIQUE_DROPRATE
-                * 100); // cus some config values are < 1 like 0.1F
-        }
-
         @Override
         public boolean canRerollNumbers() {
             return true;
@@ -60,9 +43,5 @@ public enum GearItemEnum implements IWeighted {
     public abstract boolean canGetAffixes();
 
     public abstract Words word();
-
-    public static GearItemEnum random() {
-        return RandomUtils.weightedRandom(Arrays.asList(GearItemEnum.values()));
-    }
 
 }
