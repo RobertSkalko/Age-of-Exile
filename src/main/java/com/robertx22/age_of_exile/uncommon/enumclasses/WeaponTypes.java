ad7a7802b36db8824afa1a43991794dcf09b2482
robert trebor <treborx555@gmail.com>
2021-05-23 08:47:26 +0200

    backup

--- src/main/java/com/robertx22/age_of_exile/uncommon/enumclasses/WeaponTypes.java
+++ src/main/java/com/robertx22/age_of_exile/uncommon/enumclasses/WeaponTypes.java
@@ -10,20 +10,20 @@ import java.util.stream.Collectors;
 public enum WeaponTypes {
 
     none("none", 0, PlayStyle.melee, WeaponRange.MELEE, false, 0F, 0),
-    axe("axe", 15, PlayStyle.melee, WeaponRange.MELEE, false, 1.25F, 1.25F),
-    hammer("hammer", 0, PlayStyle.melee, WeaponRange.MELEE, false, 1.5F, 0.7F),
-    mace("mace", 15, PlayStyle.melee, WeaponRange.MELEE, false, 1.2F, 0.8F),
-    staff("staff", 0, PlayStyle.magic, WeaponRange.MELEE, false, 1F, 1F),
-    dagger("dagger", 25, PlayStyle.melee, WeaponRange.MELEE, false, 0.7F, 1.5F),
-    scythe("scythe", 0, PlayStyle.magic, WeaponRange.MELEE, false, 1F, 0.75F),
-    spear("spear", 0, PlayStyle.melee, WeaponRange.MELEE, false, 1.25F, 0.75F),
+    axe("axe", 15, PlayStyle.melee, WeaponRange.MELEE, false, 1.25F, 1.5F),
+    hammer("hammer", 0, PlayStyle.melee, WeaponRange.MELEE, false, 1.5F, 1F),
+    mace("mace", 15, PlayStyle.melee, WeaponRange.MELEE, false, 1.2F, 1F),
+    staff("staff", 0, PlayStyle.magic, WeaponRange.MELEE, false, 1F, 1.2F),
+    dagger("dagger", 25, PlayStyle.melee, WeaponRange.MELEE, false, 0.7F, 1.8F),
+    scythe("scythe", 0, PlayStyle.magic, WeaponRange.MELEE, false, 1F, 1F),
+    spear("spear", 0, PlayStyle.melee, WeaponRange.MELEE, false, 1.25F, 1F),
     trident("trident", 0, PlayStyle.melee, WeaponRange.OPTIONALLY_RANGED, false, 1.2F, 1F),
-    sword("sword", 15, PlayStyle.melee, WeaponRange.MELEE, false, 0.9F, 1F),
-    glove("glove", 25, PlayStyle.melee, WeaponRange.MELEE, false, 0.8F, 1.5F),
+    sword("sword", 15, PlayStyle.melee, WeaponRange.MELEE, false, 0.9F, 1.6F),
+    glove("glove", 25, PlayStyle.melee, WeaponRange.MELEE, false, 0.8F, 1.8F),
     bow("bow", 0, PlayStyle.ranged, WeaponRange.RANGED, true, 0.8F, 1F),
     crossbow("crossbow", 0, PlayStyle.ranged, WeaponRange.RANGED, true, 0.9F, 1F),
-    wand("wand", 0, PlayStyle.magic, WeaponRange.MELEE, false, 0.9F, 1F),
-    scepter("scepter", 0, PlayStyle.magic, WeaponRange.MELEE, false, 0.9F, 1F);
+    wand("wand", 0, PlayStyle.magic, WeaponRange.MELEE, false, 0.9F, 1.2F),
+    scepter("scepter", 0, PlayStyle.magic, WeaponRange.MELEE, false, 0.9F, 1.2F);
 
     WeaponTypes(String id, int weapon_offhand_stat_util, PlayStyle style, WeaponRange range, boolean isProjectile, float statMulti, float atkPerSec) {
         this.id = id;
