194ce9edf292c4bf3db5785d83665e95e80d5e6e
mahjerion <66142256+mahjerion@users.noreply.github.com>
2021-05-27 12:17:15 -0400

    registry for bwo

--- src/main/java/com/robertx22/age_of_exile/aoe_data/database/unique_gears/uniques/UniqueWeapons.java
+++ src/main/java/com/robertx22/age_of_exile/aoe_data/database/unique_gears/uniques/UniqueWeapons.java
@@ -1,5 +1,6 @@
 package com.robertx22.age_of_exile.aoe_data.database.unique_gears.uniques;
 
+import com.robertx22.age_of_exile.aoe_data.database.unique_gears.uniques.weapons.UniqueBows;
 import com.robertx22.age_of_exile.aoe_data.database.unique_gears.uniques.weapons.UniqueSwords;
 import com.robertx22.age_of_exile.database.registry.ISlashRegistryInit;
 
