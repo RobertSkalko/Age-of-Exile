616c5de43983361bd0fe4320b47f55757c59812a
robert trebor <treborx555@gmail.com>
2021-09-11 17:12:13 +0200

    removed stat soul inv recipe, no need for it now

--- src/main/java/com/robertx22/age_of_exile/mmorpg/registers/common/ModRecipeSerializers.java
+++ src/main/java/com/robertx22/age_of_exile/mmorpg/registers/common/ModRecipeSerializers.java
@@ -2,16 +2,11 @@ package com.robertx22.age_of_exile.mmorpg.registers.common;
 
 import com.robertx22.age_of_exile.mmorpg.Ref;
 import com.robertx22.age_of_exile.player_skills.recipe_types.recipe_types.*;
-import com.robertx22.age_of_exile.saveclasses.stat_soul.StatSoulRecipe;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeSerializer;
-import net.minecraft.recipe.SpecialRecipeSerializer;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModRecipeSerializers {
-
-    public SpecialRecipeSerializer<StatSoulRecipe> STAT_SOUL_RECIPE = Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(Ref.MODID, "stat_soul"), new SpecialRecipeSerializer<>(t -> new StatSoulRecipe(t)));
 
     public RecipeSerializer<FoodShapeless> FOOD = register("food_shapeless", new FoodSerializer());
     public RecipeSerializer<TabletShapeless> TABLET = register("tablet_shapeless", new TabletSerializer());
