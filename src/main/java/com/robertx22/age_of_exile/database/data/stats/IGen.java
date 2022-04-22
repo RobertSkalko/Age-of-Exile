4713f63e0ca487eb5742022a9f139fd702ece393
Robert Skalko <treborx555@gmail.com>
2022-04-22 19:48:09 +0200

    deletes

--- src/main/java/com/robertx22/age_of_exile/database/data/stats/IGen.java
+++
@@ -1,6 +0,0 @@
-package com.robertx22.age_of_exile.database.data.stats;
-
-public interface IGen<IGUID> {
-
-    public void generateAndRegister();
-}
--- src/main/java/com/robertx22/age_of_exile/database/data/stats/types/crafting/DoNotTransferToCraftedMarker.java
+++
@@ -1,4 +0,0 @@
-package com.robertx22.age_of_exile.database.data.stats.types.crafting;
-
-public interface DoNotTransferToCraftedMarker {
-}
